4f14ca22f8a73212baddd5ee15040dccafeeab18
NoeliaAlmaraz <noelialmaraz09@gmail.com>
2024-08-14 16:20:56 +0200

    env

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { collection, getDocs, getFirestore, addDoc, doc, setDoc, updateDoc, arrayUnion } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCIfoGUtD0iQK0LEjt869WEw_A52q152wk",
-  authDomain: "gymmm-7e143.firebaseapp.com",
-  projectId: "gymmm-7e143",
-  storageBucket: "gymmm-7e143.appspot.com",
-  messagingSenderId: "890931621334",
-  appId: "1:890931621334:web:9089a440c873fa81574877"
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
